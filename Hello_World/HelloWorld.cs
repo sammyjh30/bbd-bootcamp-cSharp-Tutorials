7ee5e0050924376a817ebd17a31c9dc63c207706
Sam Hillebrand <samah@bbd.co.za>
2019-02-06 18:17:12 +0200

    Learning more about C#

---
+++ Hello_World/HelloWorld.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var aFriend = "Steve";
+            Console.WriteLine($"Hello {aFriend}!");
+            Console.WriteLine($"The name {aFriend} has {aFriend.Length} characters.");
+            //
+            string greeting = "      Hello World!       ";
+            Console.WriteLine($"[{greeting}]");
+
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+            //
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+            //
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
+            //
+            string songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+            //
+            string songLyrics2 = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics2.StartsWith("You"));
+            Console.WriteLine(songLyrics2.EndsWith("greetings"));
+        }
+    }
+}
---
+++ Numbers_In_C#/IntegerMath.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Numbers_In_C_
+{
+    class Program
+    {
+        static void IntegerMath()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+            c = a + b;
+            Console.WriteLine(c);
+        }
+
+        static void OrderOfOperands()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            a = 5;
+            b = 4;
+            c = 2;
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            a = 7;
+            b = 4;
+            c = 3;
+            d = (a + b) / c;
+            Console.WriteLine(d);
+        }
+
+        static void IntPrecisionAndLimits()
+        {
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+            //
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+            //
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Integer Math:");
+            IntegerMath();
+            Console.WriteLine("Order Of Operands:");
+            OrderOfOperands();
+            Console.WriteLine("Integer Precision And Limits");
+            IntPrecisionAndLimits();
+        }
+    }
+}
